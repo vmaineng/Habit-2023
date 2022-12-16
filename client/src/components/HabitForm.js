15fea7148ea576c96018d89c9c502699deab3e79
Mai Vang <vmaineng@gmail.com>
2022-12-15 18:35:14 -0800

    added

--- client/src/components/HabitForm.js
+++ client/src/components/HabitForm.js
@@ -1,48 +1,56 @@
-import {useState} from 'react'
+import { useState } from "react";
 
 function HabitForm() {
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [error, setError] = useState(null)
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [error, setError] = useState(null);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const habit ={ title, description}
+    const habit = { title, description };
 
-        const response = await fetch('/api/habits', {
-            method: 'POST',
-            body: JSON.stringify(habit),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        const json = await response.json()
+    const response = await fetch("/api/habits", {
+      method: "POST",
+      body: JSON.stringify(habit),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
 
-        if (!response.ok){
-            setError(json.error)
-        }
-        if (response.ok) {
-            setTitle('')
-            setDescription('')
-            setError(null)
-            console.log('new habit added')
-        }
+    if (!response.ok) {
+      setError(json.error);
     }
+    if (response.ok) {
+      setTitle("");
+      setDescription("");
+      setError(null);
+      console.log("new habit added");
+    }
+  };
 
   return (
     <div>
-        <form onSubmit={handleSubmit}>
-    <h3>Add new Habit</h3>
-    <label>Habit title:</label>
-    <input type = "text" onChange={(e) => setTitle(e.target.value)} value={title} />
-    <label>Habit Description: </label>
-    <input type = "text" onChange={(e) => setDescription(e.target.value)} value={description} />
-    <button type = "submit">Add new habit</button>
-    {error && error}
-    </form>
+      <form onSubmit={handleSubmit}>
+        <h3>Add new Habit</h3>
+        <label>Habit title:</label>
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label>Habit Description: </label>
+        <input
+          type="text"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
+        <button type="submit">Add new habit</button>
+        {error && error}
+      </form>
     </div>
-  )
+  );
 }
 
-export default HabitForm
+export default HabitForm;
