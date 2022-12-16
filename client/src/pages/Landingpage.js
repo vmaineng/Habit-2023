d06a3a61a9ac64302dc3100b3146c55c7476c988
Mai Vang <vmaineng@gmail.com>
2022-12-16 14:14:28 -0800

    footer

--- client/src/pages/Landingpage.js
+++ client/src/pages/Landingpage.js
@@ -6,6 +6,7 @@ import Purple from "../assets/purple.png";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Note from "../assets/note.png";
+import Link from '@mui/material/Link';
 
 function Landingpage() {
   return (
@@ -93,6 +94,38 @@ function Landingpage() {
           </Grid>
         </Grid>
       </Container>
+
+      {/* Footer */}
+      <Typography
+      component="footer"
+      sx={{ display: 'flex', bgcolor: 'secondary.light' }}
+    >
+
+<Container sx={{ my: 8, display: 'flex' }}>
+        <Grid container spacing={5}>
+          <Grid item xs={6} sm={4} md={3}>
+            <Grid
+              container
+              direction="column"
+              justifyContent="flex-end"
+              spacing={2}
+              sx={{ height: 120 }}
+            >
+              <Grid item xs={6} sm={4} md={2}>
+            <Typography variant="h6" marked="left" gutterBottom>
+              Legal
+            </Typography>
+            <Box component="ul" sx={{ m: 0, listStyle: 'none', p: 0 }}>
+              <Box component="li" sx={{ py: 0.5 }}>
+                <Link href="/premium-themes/onepirate/terms/">Terms</Link>
+              </Box>
+              <Box component="li" sx={{ py: 0.5 }}>
+                <Link href="/premium-themes/onepirate/privacy/">Privacy</Link>
+              </Box>
+            </Box>
+          </Grid></Grid></Grid></Grid>
+                </Container>
+        </Typography>
     </div>
   );
 }
