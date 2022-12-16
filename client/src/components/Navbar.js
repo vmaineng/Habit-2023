5d79811c8223174bce07670939488526863f9716
Mai Vang <vmaineng@gmail.com>
2022-12-16 13:50:04 -0800

    top

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,5 @@
 // import React from 'react'
-import {Link} from 'react-router-dom'
+//import {Link} from 'react-router-dom'
 
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
@@ -16,7 +16,8 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-
+//for Avatar
+import Purple from '../assets/purple.png';
 
 function Navbar() {
 
@@ -140,7 +141,7 @@ const [anchorElNav, setAnchorElNav] = React.useState(null);
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Remy Sharp" src={Purple} />
               </IconButton>
             </Tooltip>
             <Menu
--- client/src/pages/Landingpage.js
+++ client/src/pages/Landingpage.js
@@ -1,54 +1,53 @@
-import React from 'react'
-import Navbar from "../components/Navbar"
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Purple from "../assets/purple.png"
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Purple from "../assets/purple.png";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 function Landingpage() {
-
-
   return (
     <div>
-        <Navbar />
-        <Container
+      <Navbar />
+      <Container
         sx={{
           mt: 3,
           mb: 14,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-       
-{/* <Box
-          sx={{
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            backgroundColor: 'common.black',
-            opacity: 0.5,
-            zIndex: -1,
-          }}
-        /> */}
+        <Grid container spacing={5}>
+          <Grid item xs={12}>
+            <Box
+              sx={{
+                position: "absolute",
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0,
+                backgroundColor: "common.black",
+                opacity: 0.5,
+                zIndex: -1,
+              }}
+            />
 
-<Typography color="inherit" align="center" variant="h2" marked="center">
-        Habit Breaker
-      </Typography>
-         <img
-          src={Purple}
-          alt="wonder"
-          width="1400"
-          height="500"
-        />
-
-
-        </Container>
-
+            <Typography
+              color="inherit"
+              align="center"
+              variant="h2"
+              marked="center"
+            >
+              Habit Breaker
+            </Typography>
+            <img src={Purple} alt="wonder" width="1100" height="500" />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Landingpage
+export default Landingpage;
