884f47ca34bdb952f00268567f63fbc845fc064c
Mai Vang <vmaineng@gmail.com>
2022-12-15 17:28:22 -0800

    added frontend

--- server/controllers/habitController.js
+++ server/controllers/habitController.js
@@ -1,79 +1,77 @@
-const { default: mongoose } = require('mongoose');
-const Habit = require('../models/Habit');
+const { default: mongoose } = require("mongoose");
+const Habit = require("../models/Habit");
 
 //get all habits
 const getHabits = async (req, res, next) => {
-    const habits = await Habit.find({}).sort({createdAt: -1})
+  const habits = await Habit.find({}).sort({ createdAt: -1 });
 
-    res.status(200).json(habits)
-}
+  res.status(200).json(habits);
+};
 
 //get a single habit
 const getHabit = async (req, res) => {
-    const {id} = req.params
+  const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: "Not found!"})
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Not found!" });
+  }
 
-    const habit = await Habit.findById(id)
-    if (!habit) {
-        return res.status(404).json({error: 'Not Found'})
-    }
+  const habit = await Habit.findById(id);
+  if (!habit) {
+    return res.status(404).json({ error: "Not Found" });
+  }
 
-    res.status(200).json(habit)
-}
-
+  res.status(200).json(habit);
+};
 
 //create a new habit
 const createHabit = async (req, res, next) => {
-    const {title, description} = req.body
-    try {
-        const habit = await Habit.create({ title, description})
-        res.status(200).json(habit)
-    } catch(error) {
-        res.status(400).json({error: error.message})
-    }
-}
-
+  const { title, description } = req.body;
+  try {
+    const habit = await Habit.create({ title, description });
+    res.status(200).json(habit);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 //delete a single habit
 const deleteHabit = async (req, res) => {
-    const {id} = req.params
+  const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: "Not found!"})
-    }
-    
-    const habit = await Habit.findOneAndDelete({_id: id})
-    
-    if (!habit) {
-        return res.status(404).json({error: "Not found!"})
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Not found!" });
+  }
 
-    res.status(200).json({habit})
+  const habit = await Habit.findOneAndDelete({ _id: id });
 
-}
+  if (!habit) {
+    return res.status(404).json({ error: "Not found!" });
+  }
 
+  res.status(200).json({ habit });
+};
 
 //update a single habit
 const updateHabit = async (req, res) => {
-    const {id} = req.params
+  const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: "Not found!"})
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Not found!" });
+  }
+
+  const habit = await Habit.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
     }
+  );
 
-    const habit = await Habit.findOneAndUpdate({_id: id}, {
-    ...req.body
-    })
+  if (!habit) {
+    return res.status(404).json({ error: "Not found!" });
+  }
 
-    if (!habit) {
-        return res.status(404).json({error: "Not found!"})
-    }
+  res.status(200).json({ habit });
+};
 
-    res.status(200).json({habit})
-}
-
-
-module.exports = {createHabit, getHabits, getHabit, deleteHabit, updateHabit}
+module.exports = { createHabit, getHabits, getHabit, deleteHabit, updateHabit };
