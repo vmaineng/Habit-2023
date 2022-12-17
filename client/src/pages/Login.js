d68ef9f5240f4c25e2255bd033828b84e822f4ee
Mai Vang <vmaineng@gmail.com>
2022-12-16 17:54:52 -0800

    installed packages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Typography from "@mui/material/Typography";
+import {Link} from 'react-router-dom'
 
 function Login() {
   return (
     <div>
       <Navbar />
       Login
+      <Typography variant="h3" gutterBottom marked="center" align="center">
+            Sign In
+          </Typography>
+          <Typography variant="body2" align="center">
+            {'Not a member yet? '}
+            <Link
+              to="/signup"
+              align="center"
+              underline="always"
+            >
+              Sign Up here
+            </Link>
+          </Typography>
     </div>
   )
 }
