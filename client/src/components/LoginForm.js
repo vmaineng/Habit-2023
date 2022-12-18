63614a5d6c4dceb414f7978ac535cf88fa5ca09a
Mai Vang <vmaineng@gmail.com>
2022-12-17 16:32:56 -0800

    added login

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,55 @@
+import {useState} from 'react'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+// import { Field} from 'react-final-form';
+
+function LoginForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+const user = {username, password}
+
+const response = await fetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+        "Content-Type": "application/json",
+    },
+})
+
+const json = await response.json();
+
+if (!response.ok) {
+    setError(json.error)
+}
+
+if (response.ok) {
+    setUsername("");
+    setPassword("");
+    console.log("user logged in successfully");
+}
+}; 
+
+  return (
+    <div>LoginForm
+<form onSubmit={handleSubmit}> 
+<Box sx={{ mt: 6}}>
+    <label> Login</label>
+    <input type="text" onChange={(e) => setUsername(e.target.value)} value={username} />  
+    <label>Password</label>
+    <input type="text" onChange={(e) => setPassword(e.target.value)} value={password} />  
+    {/* <Field name="username" required size="large" />
+    <Field name="password" required size="large" /> */}
+   <Button type="submit">Log in</Button>
+</Box>
+</form>
+{error && error}
+    </div>
+  )
+}
+
+export default LoginForm
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,39 +1,26 @@
-import React, {useState} from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
 import Typography from "@mui/material/Typography";
-import {Link} from 'react-router-dom'
-import Box from '@mui/material/Box';
-import { Field, Form, FormSpy } from 'react-final-form';
+import { Link } from "react-router-dom";
+import LoginForm from "../components/LoginForm";
 
 function Login() {
-  const [sent, setSent] = useState(false);
-
-  const handleSubmit = () => {
-    setSent(true);
-  };
-
   return (
     <div>
       <Navbar />
-      Login
-      <Typography variant="h3" gutterBottom marked="center" align="center">
-            Sign In
-          </Typography>
-          <Typography variant="body2" align="center">
-            {'Not a member yet? '}
-            <Link
-              to="/signup"
-              align="center"
-              underline="always"
-            >
-              Sign Up here
-            </Link>
-          </Typography>
-         
-          {/* just installed React-final-form  and need to submit new user to the back*/}
 
+      <Typography variant="h3" gutterBottom marked="center" align="center">
+        Log In
+      </Typography>
+      <LoginForm />
+      <Typography variant="body2" align="center">
+        {"Not a member yet? "}
+        <Link to="/signup" align="center" underline="always">
+          Sign Up here
+        </Link>
+      </Typography>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
