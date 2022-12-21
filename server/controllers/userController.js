06aa15ac16c69a693b5b89bb07606d646e102be2
Mai Vang <vmaineng@gmail.com>
2022-12-21 11:22:23 -0800

    login works

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -108,4 +108,21 @@ const logout = async (req, res, next) => {
   res.status(200).json({ success: true })
 }
 
+
+const sendTokenResponse = (user, statusCode, res) => {
+  const token = user.getSignedJwtToken(); 
+
+  const options = {
+      expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000), 
+      httpOnly: true 
+  }
+
+  if (process.env.NODE_ENV === 'production') options.secure = true;
+
+  res
+  .status(statusCode)
+  .cookie('token', token, options)
+  .json({ success: true, token });
+} 
+
 module.exports = { createUser, getUsers, getUser, deleteUser, updateUser, login, logout};
