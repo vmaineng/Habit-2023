60a07116cbca96930b4fdbfbe9485e209d4367d3
Mai Vang <vmaineng@gmail.com>
2022-12-21 18:34:02 -0800

    started on update

--- client/src/components/HabitDetails.js
+++ client/src/components/HabitDetails.js
@@ -1,53 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   Box,
   Card,
   CardContent,
- Button,
-CardActions,
+  Button,
+  CardActions,
   Typography,
   IconButton,
   useTheme,
 } from "@mui/material";
 import FeedIcon from "@mui/icons-material/Feed";
 
-
 function HabitDetails({ habit, deleteHabit, updateHabit }) {
   const [updatedHabit, setUpdatedHabit] = useState(habit);
 
   const theme = useTheme();
 
-function handleDeleteHabit(id) {
-  fetch(`/api/habits/${id}`, {
-    method: 'DELETE',
-    headers: { Accept: 'application/json'},
-  }).then((r) => {
-    if (r.ok) {
-      deleteHabit(id);
-    }
-  });
-}
+  function handleDeleteHabit(id) {
+    fetch(`/api/habits/${id}`, {
+      method: "DELETE",
+      headers: { Accept: "application/json" },
+    }).then((r) => {
+      if (r.ok) {
+        deleteHabit(id);
+      }
+    });
+  }
 
-function handleUpdateHabit(e) {
-  e.preventDefault();
-  fetch(`/api/habits/${habit.id}`, {
-    method: 'PATCH',
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ habit: updatedHabit }),
-  })
-    .then((r) => r.json())
-    .then((updatedHabit) => {
-      updateHabit(updatedHabit);
-})
-}
-
+  function handleUpdateHabit(e) {
+    e.preventDefault();
+    fetch(`/api/habits/${habit._id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ updatedHabit}),
+    })
+      .then((r) => r.json())
+      .then((updatedHabit) => {
+        updateHabit(updatedHabit);
+      });
+  }
 
   return (
     <div>
       HabitDetail
       <Card
         sx={{
-          maxWidth: 345
+          maxWidth: 345,
         }}
       >
         <CardContent>
@@ -76,10 +74,18 @@ function handleUpdateHabit(e) {
           </Typography>
         </CardContent>
         <CardActions>
-        <Button size="small">Update</Button>
-        <button onClick={() => handleDeleteHabit(habit._id)}>Delete</button>
-      </CardActions>
-      
+          <form onSubmit={handleUpdateHabit}>
+            <input
+              type="text"
+              // name="updatedHabit"
+              placeholder="update description"
+              value={updatedHabit}
+              onChange={(e) => setUpdatedHabit(e.target.value)}
+            />
+            <Button size="small">Update</Button>
+          </form>
+          <Button onClick={() => handleDeleteHabit(habit._id)}>Delete</Button>
+        </CardActions>
       </Card>
     </div>
   );
