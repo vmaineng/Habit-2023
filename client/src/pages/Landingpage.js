ad9c304837409e960d86ba9c053b6f2703d3af24
Mai Vang <vmaineng@gmail.com>
2022-12-27 17:31:25 -0800

    added another

--- client/src/pages/Landingpage.js
+++ client/src/pages/Landingpage.js
@@ -130,8 +130,8 @@ function Landingpage() {
 
 {/* Receive offers */}
 <Container component="section" sx={{ mt: 8, display: 'flex' }}>
-     
-        <Grid item xs={12} md={6} sx={{ display: { md: 'block', xs: 'none' }, position: 'relative' }}>
+<Grid container>
+        <Grid item xs={12} md={6} sx={{ zIndex: 1 }}>
           <Box
             sx={{
               display: 'flex',
@@ -161,9 +161,41 @@ function Landingpage() {
                 Keep me updated
               </Button>
             </Box>
-          </Box>
-        
+          </Box>        
 </Grid>
+<Grid
+          item
+          xs={12}
+          md={6}
+          sx={{ display: { md: 'block', xs: 'none' }, position: 'relative' }}
+        >
+          <Box
+            sx={{
+              position: 'absolute',
+              top: -67,
+              left: -67,
+              right: 0,
+              bottom: 0,
+              width: '100%',
+              background: 'url(/static/themes/onepirate/productCTAImageDots.png)',
+            }}
+          />
+          <Box
+            component="img"
+            src="https://images.unsplash.com/photo-1527853787696-f7be74f2e39a?auto=format&fit=crop&w=750"
+            alt="call to action"
+            sx={{
+              position: 'absolute',
+              top: -28,
+              left: -28,
+              right: 0,
+              bottom: 0,
+              width: '100%',
+              maxWidth: 600,
+            }}
+          />
+        </Grid>
+        </Grid>
 </Container>
 
       <Footer />
