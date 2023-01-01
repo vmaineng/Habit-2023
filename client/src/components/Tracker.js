05592381541a64b8321fdacf79988701eede691b
Mai Vang <vmaineng@gmail.com>
2022-12-31 17:42:47 -0800

    added formatting

--- client/src/components/Tracker.js
+++ client/src/components/Tracker.js
@@ -1,27 +1,39 @@
 import React from 'react'
-import { Grid, Typography } from '@mui/material';
+import { Card,CardHeader, Box, Grid, Typography } from '@mui/material';
 
 
 function Tracker() {
   return (
     <div>
-      <Grid container rowSpacing={4.5} columnSpacing={2.75}>
+      <Grid container rowSpacing={4.5} columnSpacing={2.75} justifyContent="space-evenly">
             {/* row 1 */}
-            <Grid item xs={12} sx={{ mb: -2.25 }}>
+            <Grid item xs={12} >
                 <Typography variant="h5">Dashboard</Typography>
             </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
+            <Card>
+                <CardHeader title="Total Completions">
+            <Box sx={{ minWidth: 500}}>
                 {/* <HabitDetails title="New page" variant="h5" /> */}
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-                <Typography  variant="h5">Dashboard</Typography>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
+            </Box>
+            </CardHeader>
+            </Card>
+            <Card>
+                <CardHeader title="Current Streak">
+            <Box sx={{ minWidth: 500}}>
+                {/* <HabitDetails title="New page" variant="h5" /> */}
+            </Box>
+            </CardHeader>
+            </Card>
+            <Card>
+                <CardHeader title="Longest Streak">
+            <Box sx={{ minWidth: 500}}>
+                {/* <HabitDetails title="New page" variant="h5" /> */}
+            </Box>
+            </CardHeader>
+            </Card>
+            {/* <Grid item xs={12} sm={6} md={4} lg={3}>
             <Typography  variant="h5">Dashboard</Typography>
-            </Grid>
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Typography  variant="h5">Dashboard</Typography>
-            </Grid>
+            </Grid> */}
             </Grid>
 
             
