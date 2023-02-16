884196ad567569900a1ae6ec1322daf1ffecc608
Mai Vang <vmaineng@gmail.com>
2023-02-16 14:42:13 -0800

    updated db link

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({path: '.env'});
+require("dotenv").config(({ path: "./config/config.env"}));
 
 // const { application } = require('express');
 const mongoose = require("mongoose");
